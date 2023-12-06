f52aa22bcecc314e26e26a19479d3ba1903444a1
John Pickup <home@johnpickup.com>
2023-12-06 07:55:40 +0000

    attempt a more mathematical approach

--- src/com/johnpickup/aoc2023/Day6.java
+++ src/com/johnpickup/aoc2023/Day6.java
@@ -18,15 +18,15 @@ public class Day6 {
         try (Stream<String> stream = Files.lines(Paths.get("/Users/john/Development/AdventOfCode/resources/2023/Day6.txt"))) {
             List<String> lines = stream.filter(s -> !s.isEmpty()).collect(Collectors.toList());
             // Part 1
-            //List<Long> times = Arrays.stream(lines.get(0).replace(" ","").split(":")[1].split(" ")).filter(s -> !s.isEmpty()).map(Long::parseLong).collect(Collectors.toList());
-            //List<Long> distances = Arrays.stream(lines.get(1).replace(" ","").split(":")[1].split(" ")).filter(s -> !s.isEmpty()).map(Long::parseLong).collect(Collectors.toList());
+            //List<Long> times = Arrays.stream(lines.get(0).split(":")[1].split(" ")).filter(s -> !s.isEmpty()).map(Long::parseLong).collect(Collectors.toList());
+            //List<Long> distances = Arrays.stream(lines.get(1).split(":")[1].split(" ")).filter(s -> !s.isEmpty()).map(Long::parseLong).collect(Collectors.toList());
             // Part2 - expected this not to work without significant changes, as is often the case, but it completes in 165ms, i.e. no optimisation required
             List<Long> times = Arrays.stream(lines.get(0).replace(" ","").split(":")[1].split(" ")).filter(s -> !s.isEmpty()).map(Long::parseLong).collect(Collectors.toList());
             List<Long> distances = Arrays.stream(lines.get(1).replace(" ","").split(":")[1].split(" ")).filter(s -> !s.isEmpty()).map(Long::parseLong).collect(Collectors.toList());
 
             long result = IntStream.range(0, times.size())
                     .mapToObj(i -> new RaceRecord(times.get(i), distances.get(i)))
-                    .map(RaceRecord::waysToWin)
+                    .map(RaceRecord::waysToWinMaths)
                     .reduce(1L, (a,b) -> a*b);
             System.out.println(result);
         } catch (IOException e) {
@@ -49,6 +49,17 @@ public class Day6 {
                  if (d > record) result++;
             }
             return result;
+        }
+
+        // This doesn't quite work - can't find a way to round to longs correctly for all cases
+        public long waysToWinMaths() {
+            double quad = Math.sqrt(1d * time * time - 4d * record) / -2d;
+            double v1 =  quad;
+            double v2 =  - quad;
+            System.out.println(String.format("%f %f", v1, v2));
+
+            //return (long)(Math.floor(v2) - Math.ceil(v1));
+            return Math.round(v2 - v1);
         }
     }
 }
