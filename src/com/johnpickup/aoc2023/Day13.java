935c4f4144c076409c7b8517225bf12d7b15591a
John Pickup <home@johnpickup.com>
2023-12-13 18:30:18 +0000

    Day 13 - working

--- src/com/johnpickup/aoc2023/Day13.java
+++ src/com/johnpickup/aoc2023/Day13.java
@@ -8,14 +8,13 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Day13 {
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
-        try (Stream<String> stream = Files.lines(Paths.get("/Users/john/Development/AdventOfCode/resources/2023/Day13-test.txt"))) {
+        try (Stream<String> stream = Files.lines(Paths.get("/Users/john/Development/AdventOfCode/resources/2023/Day13.txt"))) {
             List<String> lines = stream.collect(Collectors.toList());
             List<String> boardLines = new ArrayList<>();
             List<Board> boards = new ArrayList<>();
@@ -32,17 +31,27 @@ public class Day13 {
             }
 
             long part1 = 0;
+            long part2 = 0;
 
             for (Board board : boards) {
-                System.out.println(board);
                 int reflectRow = board.getReflectRow();
                 int reflectCol = board.getReflectCol();
-                System.out.println("row:" + reflectRow + " ; col: " + reflectCol);
-
                 part1 += (long)reflectCol + 100L * reflectRow;
+                if (reflectRow > 0) {
+                    // find the col with a single difference
+                    reflectRow = board.getSingleDiffReflectRow();
+                    reflectCol = board.getSingleDiffReflectCol();
+                }
+                else {
+                    // find the row with a single difference
+                    reflectCol = board.getSingleDiffReflectCol();
+                    reflectRow = board.getSingleDiffReflectRow();
+                }
+                part2 += (long)reflectCol + 100L * reflectRow;
             }
 
             System.out.println("Part 1 : " + part1);
+            System.out.println("Part 2 : " + part2);
 
 
         } catch (IOException e) {
@@ -54,10 +63,33 @@ public class Day13 {
 
     @RequiredArgsConstructor
     @Data
+    static class Coord {
+        final int x;
+        final int y;
+
+        public Coord reflectX(int reflectAt) {
+            int before = (reflectAt - this.x);
+            int newX = reflectAt + before + 1;
+            return new Coord(newX, this.y);
+        }
+
+        public Coord reflectY(int reflectAt) {
+            int before = (reflectAt - this.y);
+            int newY = reflectAt + before + 1;
+            return new Coord(this.x, newY);
+        }
+
+    }
+    @RequiredArgsConstructor
+    @Data
     static class Board {
         final Cell[][] cells;
         final int width;
         final int height;
+
+        Cell getCell(Coord coord) {
+            return getCell(coord.x, coord.y);
+        }
 
         Cell getCell(int col, int row) {
             if (col < 0 || col >= width) return Cell.UNKNOWN;
@@ -76,36 +108,64 @@ public class Day13 {
         }
 
         public int getReflectRow() {
-            return 0;
-        }
-
-        public int getReflectCol() {
-            for (int col = 1; col < width; col++) {
+            for (int reflectRow = 0; reflectRow < height-1; reflectRow++) {
                 boolean allMatch = true;
-                // do all cols to reflect either side?
-                System.out.println("Checking " +col);
-                for (int i=0; i < width; i++) {
-                    int x1 = col - (i-1);
-                    int x2 = col + i;
-                    System.out.println(x1 + " vs " +x2);
-
-                    /*
-                    on 5
-                    5 -> 6   (x - col) + 1
-                    4 -> 7
-                    3 -> 8
-                    2 -> 9
-                     */
-
-                    for (int row=0; row < height; row++) {
-                        allMatch &= getCell(x1, row) == getCell(x2, row);
+                for (int x=0; x < width; x++) {
+                    for (int y=0; y < height; y++) {
+                        Coord c1 = new Coord(x, y);
+                        Coord c2 = c1.reflectY(reflectRow);
+                        allMatch &= getCell(c1).reflects(getCell(c2));
                     }
                 }
-                if (allMatch) return col+1;
+                if (allMatch) return reflectRow+1;
             }
             return 0;
         }
 
+        public int getReflectCol() {
+            for (int reflectCol = 0; reflectCol < width-1; reflectCol++) {
+                boolean allMatch = true;
+                for (int x=0; x < width; x++) {
+                    for (int y=0; y < height; y++) {
+                        Coord c1 = new Coord(x, y);
+                        Coord c2 = c1.reflectX(reflectCol);
+                        allMatch &= getCell(c1).reflects(getCell(c2));
+                    }
+                }
+                if (allMatch) return reflectCol+1;
+            }
+            return 0;
+        }
+
+        public int getSingleDiffReflectCol() {
+            for (int reflectCol = 0; reflectCol < width-1; reflectCol++) {
+                int diffCount = 0;
+                for (int x=0; x < width; x++) {
+                    for (int y=0; y < height; y++) {
+                        Coord c1 = new Coord(x, y);
+                        Coord c2 = c1.reflectX(reflectCol);
+                        if (!getCell(c1).reflects(getCell(c2))) diffCount++;
+                    }
+                }
+                if (diffCount == 2) return reflectCol+1;  // every diff is doubled as we scan every cell
+            }
+            return 0;
+        }
+
+        public int getSingleDiffReflectRow() {
+            for (int reflectRow = 0; reflectRow < height-1; reflectRow++) {
+                int diffCount = 0;
+                for (int x=0; x < width; x++) {
+                    for (int y=0; y < height; y++) {
+                        Coord c1 = new Coord(x, y);
+                        Coord c2 = c1.reflectY(reflectRow);
+                        if (!getCell(c1).reflects(getCell(c2))) diffCount++;
+                    }
+                }
+                if (diffCount == 2) return reflectRow+1;  // every diff is doubled as we scan every cell
+            }
+            return 0;
+        }
     }
 
     enum Cell {
@@ -121,8 +181,7 @@ public class Day13 {
         }
 
         public boolean reflects(Cell other) {
-            return other==UNKNOWN || this.equals(other);
+            return this==UNKNOWN || other==UNKNOWN || this.equals(other);
         }
     }
-
 }
