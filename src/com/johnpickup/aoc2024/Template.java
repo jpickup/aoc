effc63e8877eee8166f8587ffe079561b11f79fc
John Pickup <home@johnpickup.com>
2024-12-14 10:46:20 +0000

    Save literal seconds by automatically creating input dir & files

---
+++ src/com/johnpickup/aoc2024/Template.java
@@ -0,0 +1,44 @@
+package com.johnpickup.aoc2024;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static com.johnpickup.aoc2024.util.FileUtils.createEmptyTestFileIfMissing;
+
+public class Template {
+    public static void main(String[] args) {
+        // assume input files are in a directory that matches
+        String day = new Object() { }.getClass().getEnclosingClass().getSimpleName();
+        String prefix = "/Volumes/User Data/john/Development/AdventOfCode/resources/2024/" + day + "/" + day;
+        List<String> inputFilenames = Arrays.asList(
+                prefix + "-test.txt"
+                , prefix + ".txt"
+        );
+        for (String inputFilename : inputFilenames) {
+            createEmptyTestFileIfMissing(inputFilename);
+            long start = System.currentTimeMillis();
+            System.out.println(inputFilename);
+            try (Stream<String> stream = Files.lines(Paths.get(inputFilename))) {
+                List<String> lines = stream
+                        .filter(s -> !s.isEmpty())
+                        .collect(Collectors.toList());
+
+                long part1 = 0L;
+                System.out.println("Part 1: " + part1);
+                long part2 = 0L;
+                System.out.println("Part 2: " + part2);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            long end = System.currentTimeMillis();
+            System.out.println("Time: " + (end - start) + "ms");
+        }
+    }
+
+}
---
+++ src/com/johnpickup/aoc2024/util/FileUtils.java
@@ -0,0 +1,25 @@
+package com.johnpickup.aoc2024.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class FileUtils {
+    public static void createEmptyTestFileIfMissing(String filename) {
+        try {
+        File file = new File(filename);
+        File dir = file.getParentFile();
+        if (!dir.exists()) {
+            System.out.println("Creating directory " + dir);
+            Files.createDirectory(dir.toPath());
+        }
+        if (!file.exists()) {
+            System.out.println("Creating file " + file);
+            Files.createFile(file.toPath());
+        }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
