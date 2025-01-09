24c09823afcee2020562efb8f001ff0d31bc1e52
John Pickup <home@johnpickup.com>
2025-01-09 20:14:42 +0000

    Day 13 - part 2

--- src/com/johnpickup/aoc2019/Day13.java
+++ src/com/johnpickup/aoc2019/Day13.java
@@ -2,6 +2,8 @@ package com.johnpickup.aoc2019;
 
 import com.johnpickup.util.Coord;
 import com.johnpickup.util.SparseGrid;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -36,10 +38,13 @@ public class Day13 {
                 ArcadeCabinet arcadeCabinet = new ArcadeCabinet(lines.get(0));
                 arcadeCabinet.execute();
                 System.out.println(arcadeCabinet);
-
                 long part1 = arcadeCabinet.part1();
                 System.out.println("Part 1: " + part1);
-                long part2 = 0L;
+
+                arcadeCabinet.reset();
+                arcadeCabinet.insertCoins(2);
+                arcadeCabinet.execute();
+                long part2 = arcadeCabinet.getScore();
                 System.out.println("Part 2: " + part2);
 
             } catch (IOException e) {
@@ -54,20 +59,38 @@ public class Day13 {
         final Program program;
         final SparseGrid<Integer> screen;
 
+        private Joystick joystick;
+
+        @Getter
+        private long score = 0;
+        private final Coord scoreCoord = new Coord(-1,0);
+
+        private Coord ballPosition = Coord.ORIGIN;
+        private Coord paddlePosition = Coord.ORIGIN;
+
         ArcadeCabinet(String line) {
             program = new Program(line);
+            program.inputSupplier = this::supplyInput;
             program.outputConsumer = this::processOutput;
             screen = new SparseGrid<>();
         }
 
-        public void execute() {
+        public void reset() {
             program.reset();
+            joystick = Joystick.MIDDLE;
+        }
+
+        public void execute() {
             program.execute();
         }
 
         @Override
         public String toString() {
             return screen.toString();
+        }
+
+        private long supplyInput() {
+            return joystick.position;
         }
 
         private final List<Integer> outputQueue = new ArrayList<>();
@@ -80,12 +103,36 @@ public class Day13 {
                 outputQueue.remove(2);
                 outputQueue.remove(1);
                 outputQueue.remove(0);
-                screen.setCell(new Coord(x, y), t);
+                Coord coord = new Coord(x, y);
+                if (coord.equals(scoreCoord)) {
+                    score = t;
+                } else {
+                    screen.setCell(coord, t);
+                    switch (t) {
+                        case 3: paddlePosition = coord; break;
+                        case 4: ballPosition = coord; break;
+                    }
+                }
+                joystick = Joystick.MIDDLE;
+                if (ballPosition.getX() < paddlePosition.getX()) joystick = Joystick.LEFT;
+                if (ballPosition.getX() > paddlePosition.getX()) joystick = Joystick.RIGHT;
             }
         }
 
         public long part1() {
             return screen.getCells().values().stream().filter(v -> v == 2).count();
         }
+
+        public void insertCoins(int number) {
+            program.setMemory(0, number);
+        }
+    }
+
+    @RequiredArgsConstructor
+    enum Joystick {
+        LEFT(-1),
+        MIDDLE(0),
+        RIGHT(1);
+        final long position;
     }
 }
