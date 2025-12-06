483e862cabab64efd0488923d4e22fe7d883c20c
jwpickup <home@johnpickup.com>
2025-12-06 07:25:45 +0530

    2025 - rnge tests

--- test/com/johnpickup/util/RangeTest.java
+++ test/com/johnpickup/util/RangeTest.java
@@ -2,6 +2,8 @@ package com.johnpickup.util;
 
 import org.junit.Test;
 
+import java.util.Collection;
+
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
@@ -28,5 +30,57 @@ public class RangeTest {
         Range<Integer> r2 = new Range<>(3,4);
         assertThat(r1.intersection(r2), is(equalTo(new Range<>(3,4))));
         assertThat(r2.intersection(r1), is(equalTo(new Range<>(3,4))));
+    }
+
+    @Test
+    public void combineFirstContainsSecond() {
+        Range<Integer> r1 = new Range<>(1,6);
+        Range<Integer> r2 = new Range<>(3,4);
+        Collection<Range<Integer>> combined = Range.combine(r1, r2);
+        assertThat(combined.size(), is(1));
+        Range<Integer> single = combined.stream().findFirst().orElseThrow();
+        assertThat(single.getLower(), is(1));
+        assertThat(single.getUpper(), is(6));
+    }
+
+    @Test
+    public void combineSecondContainsFirst() {
+        Range<Integer> r1 = new Range<>(3,4);
+        Range<Integer> r2 = new Range<>(1,6);
+        Collection<Range<Integer>> combined = Range.combine(r1, r2);
+        assertThat(combined.size(), is(1));
+        Range<Integer> single = combined.stream().findFirst().orElseThrow();
+        assertThat(single.getLower(), is(1));
+        assertThat(single.getUpper(), is(6));
+    }
+
+    @Test
+    public void combineOverlap() {
+        Range<Integer> r1 = new Range<>(1,4);
+        Range<Integer> r2 = new Range<>(3,6);
+        Collection<Range<Integer>> combined = Range.combine(r1, r2);
+        assertThat(combined.size(), is(1));
+        Range<Integer> single = combined.stream().findFirst().orElseThrow();
+        assertThat(single.getLower(), is(1));
+        assertThat(single.getUpper(), is(6));
+    }
+
+    @Test
+    public void combineDisjoint() {
+        Range<Integer> r1 = new Range<>(1,3);
+        Range<Integer> r2 = new Range<>(4,6);
+        Collection<Range<Integer>> combined = Range.combine(r1, r2);
+        assertThat(combined.size(), is(2));
+    }
+
+    @Test
+    public void combineDAdjacent() {
+        Range<Integer> r1 = new Range<>(1,3);
+        Range<Integer> r2 = new Range<>(3,6);
+        Collection<Range<Integer>> combined = Range.combine(r1, r2);
+        assertThat(combined.size(), is(1));
+        Range<Integer> single = combined.stream().findFirst().orElseThrow();
+        assertThat(single.getLower(), is(1));
+        assertThat(single.getUpper(), is(6));
     }
 }
