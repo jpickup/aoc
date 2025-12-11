ef63b2b33070ec21e14ab3c825b39203aa0bdf29
John Pickup <home@johnpickup.com>
2025-12-11 16:45:15 +0000

    Day 11 - very messy - could benefit from a big tidy-up

--- src/com/johnpickup/aoc2025/Day11Groups.java
+++ src/com/johnpickup/aoc2025/Day11Groups.java
@@ -1,5 +1,6 @@
 package com.johnpickup.aoc2025;
 
+import com.johnpickup.util.Pair;
 import lombok.EqualsAndHashCode;
 import lombok.RequiredArgsConstructor;
 
@@ -12,7 +13,7 @@ import java.util.stream.Stream;
 
 import static com.johnpickup.util.FileUtils.getInputFilenames;
 
-public class Day11 {
+public class Day11Groups {
     static boolean isTest;
     public static void main(String[] args) {
         List<String> inputFilenames = getInputFilenames(new Object(){});
@@ -37,22 +38,72 @@ public class Day11 {
                 Node dac = nodeMap.get("dac");
                 Node fft = nodeMap.get("fft");
 
-                long part1 = findPaths(startNode, out, 7).size();
+                long part1 = findPaths(startNode, out).size();
                 System.out.println("Part 1: " + part1);
 
-                Node middle = nodeMap.get("oxe");  //yzf, qzf, exf, oxe
-                // 79034316383680
-                // 173267997409464
-                // 82052969827692
-                // 33224358134844
-
-                long startFft = findPaths(svr, fft, 10).size();
-                    long fftMid = findPaths(fft, middle, 10).size();
-                    long midDac = findPaths(middle, dac, 10).size();
-                    long dacOut = findPaths(dac, out, 10).size();
-                    long part2 = startFft * fftMid * midDac * dacOut;
+                if (isTest) {
+                    long startDac = findPaths(svr, dac).size();
+                    long startFft = findPaths(svr, fft).size();
+                    long dacFft = findPaths(dac, fft).size();
+                    long fftDac = findPaths(fft, dac).size();
+                    long fftOut = findPaths(fft, out).size();
+                    long dacOut = findPaths(dac, out).size();
+                    long part2 = startDac*dacFft*fftOut + startFft * fftDac * dacOut;
                     System.out.println("Part 2: " + part2);
+                } else {
+                    List<List<String>> stages = new ArrayList<>();
+                    // visual inspection of the graph shows these key stages
+                    stages.add(Collections.singletonList("svr"));
+                    stages.add(Arrays.asList("cno", "afb", "bya", "mfc", "yag"));
+                    stages.add(Arrays.asList("vrb", "myd", "fkk"));
+                    stages.add(Arrays.asList("yzf", "qzf", "exf", "oxe"));
+                    stages.add(Arrays.asList("weg", "kso", "dgp"));
+                    stages.add(Arrays.asList("gbc", "hkx", "lpt", "you"));
+                    stages.add(Collections.singletonList("out"));
 
+                    // "fft" is in 2nd group, e.g. cno to vrb - need to filter those
+                    // "dac" is in the 5th group, e.g.weg to gbc
+                    List<String> fftGroupStarts = Arrays.asList("cno", "afb", "bya", "mfc", "yag");
+                    List<String> dacGroupStarts = Arrays.asList("weg", "kso", "dgp");
+
+                    List<String> prevStage = null;
+                    Map<Pair<String, String>, Long> stageCounts = new HashMap<>();
+
+                    long part2 = 1L;
+
+                    for (List<String> stage : stages) {
+                        long stageTotal = 0L;
+                        if (prevStage != null) {
+                            for (String previous : prevStage) {
+                                for (String next : stage) {
+                                    System.out.printf("Paths from %s to %s: ", previous, next);
+                                    Set<List<Node>> paths = findPaths(nodeMap.get(previous), nodeMap.get(next));
+                                    if (fftGroupStarts.contains(previous)) {
+                                        System.out.printf("Pre-filter: %d paths, ", paths.size());
+                                        System.out.print("Filtering for FFT, ");
+                                        paths = paths.stream().filter(p -> p.contains(fft)).collect(Collectors.toSet());
+                                    }
+                                    if (dacGroupStarts.contains(previous)) {
+                                        System.out.printf("Pre-filter: %d paths, ", paths.size());
+                                        System.out.print("Filtering for DAC, ");
+                                        paths = paths.stream().filter(p -> p.contains(dac)).collect(Collectors.toSet());
+                                    }
+                                    System.out.printf("Post filter: %d paths%n", paths.size());
+
+                                    stageCounts.put(new Pair<>(previous, next), (long)paths.size());
+                                    stageTotal += paths.size();
+                                }
+                            }
+                            System.out.printf("Stage total = %d%n", stageTotal);
+                            part2 *= stageTotal;
+                            System.out.println("Intermediate part 2: " + part2);
+                        }
+                        prevStage = stage;
+                    }
+                    System.out.println("Part 2: " + part2);
+                    // 298708811351838720 too high
+                    // 118981638357120000 too high
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -74,7 +125,7 @@ public class Day11 {
         return sb.toString();
     }
 
-    private static Set<List<Node>> findPaths(Node startNode, Node endNode, int maxLength) {
+    private static Set<List<Node>> findPaths(Node startNode, Node endNode) {
         if (startNode==null) return Collections.emptySet();
         if (endNode==null) return Collections.emptySet();
         // bfs
@@ -83,7 +134,7 @@ public class Day11 {
         paths.add(Collections.singletonList(startNode));
         boolean madeProgress = true;
         while (madeProgress) {
-            Set<List<Node>> newPaths = explore(paths, maxLength);
+            Set<List<Node>> newPaths = explore(paths);
             Set<List<Node>> solutions = newPaths.stream().filter(p -> isSolution(p, endNode)).collect(Collectors.toSet());
             result.addAll(solutions);
             newPaths.removeAll(solutions);
@@ -94,12 +145,12 @@ public class Day11 {
         return result;
     }
 
-    private static Set<List<Node>> explore(Set<List<Node>> paths, int maxLength) {
+    private static Set<List<Node>> explore(Set<List<Node>> paths) {
         Set<List<Node>> result = new HashSet<>();
         for (List<Node> path : paths) {
             Node last = path.getLast();
             for (Node outputNode : last.outputNodes) {
-                if (!path.contains(outputNode) && path.size() <= maxLength) {
+                if (!path.contains(outputNode) && path.size() <= 7) {
                     List<Node> extendedPath = new ArrayList<>(path);
                     extendedPath.add(outputNode);
                     result.add(extendedPath);
