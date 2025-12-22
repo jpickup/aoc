9504a3f647914d707702243a72ff174d7e1ca67b
John Pickup <home@johnpickup.com>
2025-12-22 10:22:01 +0000

    day 11 - better

--- src/com/johnpickup/aoc2025/Day11.java
+++ src/com/johnpickup/aoc2025/Day11.java
@@ -1,5 +1,6 @@
 package com.johnpickup.aoc2025;
 
+import com.johnpickup.util.Pair;
 import lombok.EqualsAndHashCode;
 import lombok.RequiredArgsConstructor;
 
@@ -15,7 +16,7 @@ import static com.johnpickup.util.FileUtils.getInputFilenames;
 public class Day11 {
     static boolean isTest;
     public static void main(String[] args) {
-        List<String> inputFilenames = getInputFilenames(new Object(){});
+        List<String> inputFilenames = getInputFilenames("2025", "Day11");
         for (String inputFilename : inputFilenames) {
             
             long start = System.currentTimeMillis();
@@ -37,22 +38,71 @@ public class Day11 {
                 Node dac = nodeMap.get("dac");
                 Node fft = nodeMap.get("fft");
 
-                long part1 = findPaths(startNode, out, 7).size();
+                long part1 = findPaths(startNode, out).size();
                 System.out.println("Part 1: " + part1);
 
-                Node middle = nodeMap.get("oxe");  //yzf, qzf, exf, oxe
-                // 79034316383680
-                // 173267997409464
-                // 82052969827692
-                // 33224358134844
-
-                long startFft = findPaths(svr, fft, 10).size();
-                    long fftMid = findPaths(fft, middle, 10).size();
-                    long midDac = findPaths(middle, dac, 10).size();
-                    long dacOut = findPaths(dac, out, 10).size();
-                    long part2 = startFft * fftMid * midDac * dacOut;
+                if (isTest) {
+                    long startDac = findPaths(svr, dac).size();
+                    long startFft = findPaths(svr, fft).size();
+                    long dacFft = findPaths(dac, fft).size();
+                    long fftDac = findPaths(fft, dac).size();
+                    long fftOut = findPaths(fft, out).size();
+                    long dacOut = findPaths(dac, out).size();
+                    long part2 = startDac * dacFft * fftOut + startFft * fftDac * dacOut;
                     System.out.println("Part 2: " + part2);
+                } else {
+                    List<List<String>> stages = new ArrayList<>();
+                    // visual inspection of the graph shows these key stages
+                    stages.add(Collections.singletonList("svr"));
+                    stages.add(Arrays.asList("cno", "afb", "bya", "mfc", "yag"));
+                    stages.add(Collections.singletonList("fft"));       // add an intermediate stage to go through fft
+                    stages.add(Arrays.asList("vrb", "myd", "fkk"));
+                    stages.add(Arrays.asList("yzf", "qzf", "exf", "oxe"));
+                    stages.add(Arrays.asList("weg", "kso", "dgp"));
+                    stages.add(Collections.singletonList("dac"));       // add an intermediate stage to go through dac
+                    stages.add(Arrays.asList("gbc", "hkx", "lpt", "you"));
+                    stages.add(Collections.singletonList("out"));
 
+                    List<String> prevStage = null;
+                    Map<Pair<String, String>, Long> stageCounts = new HashMap<>();
+
+                    for (List<String> stage : stages) {
+                        if (prevStage != null) {
+                            for (String previous : prevStage) {
+                                for (String next : stage) {
+                                    System.out.printf("Paths from %s to %s: ", previous, next);
+                                    Set<List<Node>> paths = findPaths(nodeMap.get(previous), nodeMap.get(next));
+                                    System.out.printf("%d paths%n", paths.size());
+
+                                    stageCounts.put(new Pair<>(previous, next), (long) paths.size());
+                                }
+                            }
+                        }
+                        prevStage = stage;
+                    }
+                    // multiply the combinations together
+                    // If we have groups A -> B,C | B,C -> D,E | D,E -> F
+                    // then we need:
+                    // (A->B * B->D * D->F) + (A->C * C->D * D->F) +
+                    // (A->B * B->E * E->F) + (A->C * C->E * E->F)
+                    prevStage = null;
+                    Map<String, Long> pathCountToNode = new HashMap<>();
+                    pathCountToNode.put("svr", 1L);
+                    for (List<String> stage : stages) {
+                        if (prevStage != null) {
+                            for (String previous : prevStage) {
+                                for (String next : stage) {
+                                    Long stageCount = stageCounts.get(new Pair<>(previous, next));
+                                    Long prevPathCount = pathCountToNode.get(previous);
+                                    Long nextPathCount = pathCountToNode.getOrDefault(next, 0L) + prevPathCount * stageCount;
+                                    pathCountToNode.put(next, nextPathCount);
+                                }
+                            }
+                        }
+                        prevStage = stage;
+                    }
+                    System.out.println("Part 2: " + pathCountToNode.get("out"));
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -74,7 +124,7 @@ public class Day11 {
         return sb.toString();
     }
 
-    private static Set<List<Node>> findPaths(Node startNode, Node endNode, int maxLength) {
+    private static Set<List<Node>> findPaths(Node startNode, Node endNode) {
         if (startNode==null) return Collections.emptySet();
         if (endNode==null) return Collections.emptySet();
         // bfs
@@ -83,7 +133,7 @@ public class Day11 {
         paths.add(Collections.singletonList(startNode));
         boolean madeProgress = true;
         while (madeProgress) {
-            Set<List<Node>> newPaths = explore(paths, maxLength);
+            Set<List<Node>> newPaths = explore(paths);
             Set<List<Node>> solutions = newPaths.stream().filter(p -> isSolution(p, endNode)).collect(Collectors.toSet());
             result.addAll(solutions);
             newPaths.removeAll(solutions);
@@ -94,12 +144,14 @@ public class Day11 {
         return result;
     }
 
-    private static Set<List<Node>> explore(Set<List<Node>> paths, int maxLength) {
+    private static final int MAX_LENGTH = 8;
+
+    private static Set<List<Node>> explore(Set<List<Node>> paths) {
         Set<List<Node>> result = new HashSet<>();
         for (List<Node> path : paths) {
             Node last = path.getLast();
             for (Node outputNode : last.outputNodes) {
-                if (!path.contains(outputNode) && path.size() <= maxLength) {
+                if (!path.contains(outputNode) && path.size() <= MAX_LENGTH) {
                     List<Node> extendedPath = new ArrayList<>(path);
                     extendedPath.add(outputNode);
                     result.add(extendedPath);
